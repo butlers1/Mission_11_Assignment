112b420125e502275e65583b2137321db7d5c4a3
butlers1 <butlers1@byu.edu>
2026-03-31 17:43:19 -0600

    mission 12 final

---
+++ frontend/src/types.ts
@@ -0,0 +1,22 @@
+// src/types.ts
+export interface Book {
+    bookId: number;
+    title: string;
+    author: string;
+    publisher: string;
+    isbn: string;
+    classification: string;
+    category: string;
+    pageCount: number;
+    price: number;
+}
+
+export interface CartItem {
+    book: Book;
+    quantity: number;
+}
+
+export interface BooksResponse {
+    books: Book[];
+    totalCount: number;
+}
